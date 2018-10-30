ae509c0e9cf43c7f9ee2ee9c1bc40645ed022291
davidhm0 <davidhm0@yahoo.es>
2018-10-30 06:19:13 +0100

    Hacer login en Firebase y pedir lista de libros

--- app/src/main/java/com/davidhm/pqtm/mybooks/BookListActivity.java
+++ app/src/main/java/com/davidhm/pqtm/mybooks/BookListActivity.java
@@ -1,7 +1,11 @@
 package com.davidhm.pqtm.mybooks;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,15 +13,23 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.davidhm.pqtm.mybooks.model.BookContent;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -38,9 +50,16 @@ public class BookListActivity extends AppCompatActivity {
      */
     private boolean mTwoPane;
 
-    // Declara instancias de Firebase
+    // Instancias de autenticación y base de datos de Firebase
     private FirebaseAuth mAuth;
     private FirebaseDatabase database;
+
+    // Parámetros para autenticación en Firebase
+    private static final String mEmail = "davidhm0@yahoo.es";
+    private static final String mPassword = "pqtm-davidhm";
+
+    // Etiqueta para Logs
+    private static final String TAG = "MyBooks";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +70,7 @@ public class BookListActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
 
+        // Configura elementos de la UI
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle(getTitle());
@@ -75,6 +95,105 @@ public class BookListActivity extends AppCompatActivity {
         View recyclerView = findViewById(R.id.book_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
+
+        // ============ INICIO CODIGO A COMPLETAR (ejercicio 2) ===============
+
+        // Comprueba si hay acceso a la Red
+        if (!isNetworkConnected()) {
+            // No hay acceso -> muestra un mensaje al usuario
+            Log.d(TAG, "onCreate:no hay acceso a la red");
+            showMessage("NO HAY ACCESO A LA RED");
+        } else {
+            // Comprueba si el usuario ya está autenticado en Firebase
+            if (mAuth.getCurrentUser() == null) {
+                // No hay mingún usuario autenticado -> intenta hacer login
+                signIn(mEmail, mPassword);
+            } else {
+                // El usuario ya está autenticado -> pide libros al servidor
+                Log.d(TAG, "onCreate:usuario autenticado previamente");
+                getFirebaseBookList();
+            }
+        }
+
+        // ============ FIN CODIGO A COMPLETAR ===============
+    }
+
+    /**
+     * Comprueba si hay acceso a la red.
+     *
+     * @return  true si hay acceso a la red; false en otro caso
+     */
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return (netInfo != null && netInfo.isConnected() && netInfo.isAvailable());
+    }
+
+    /**
+     * Muestra en la pantalla un cuadro de diálogo con un mensaje.
+     *
+     * @param msg   el mensaje a mostrar
+     */
+    public void showMessage(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(BookListActivity.this);
+        builder.setMessage(msg).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) { }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    /**
+     * Intenta hacer login en el servidor Firebase.
+     * Si la autenticación tiene éxito, pide la lista de libros al servidor.
+     *
+     * @param email     email de autenticación en Firebase
+     * @param password  contraseña de autenticación en Firebase
+     */
+    private void signIn(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Autenticación en Firebase correcta -> pide libros al servidor
+                            Log.d(TAG, "signIn:usuario autenticado correctamente");
+                            getFirebaseBookList();
+                        } else {
+                            // La autenticación falla -> muestra un mensaje al usuario.
+                            Log.w(TAG, "signIn:error de autenticación", task.getException());
+                            Toast.makeText(BookListActivity.this,
+                                    "ERROR DE AUTENTICACIÓN EN FIREBASE", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
+    }
+
+    /**
+     * Pide la lista de libros al servidor Firebase, y asigna un listener a
+     * la referencia obtenida. El método onDataChange es invocado por primera
+     * vez cuando se asigna el listener, y cada vez que hay modificaciones en
+     * los datos a los que apunta la referencia.
+     */
+    private void getFirebaseBookList() {
+        database.getReference("books").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // Recibe modificaciones de la lista de libros de Firebase
+                // Muestra un mensaje.
+                Log.d(TAG, "onDataChange:recibida lista de libros de Firebase");
+                Toast.makeText(BookListActivity.this, "Recibida lista de libros de Firebase",
+                        Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Error en el acceso a la base de datos Firebase.
+                Log.w(TAG, "onCancelled:error en acceso a base de datos de Firebase", databaseError.toException());
+                Toast.makeText(BookListActivity.this, "Error en acceso a base de datos de Firebase",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
